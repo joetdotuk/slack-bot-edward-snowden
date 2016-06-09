38384e9d4835fd0624ab41ed35bc61fcbfb8c51b
joetdotuk <joe@joet.uk>
2016-06-09 18:07:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function onMessageToEdwardSnowden (message) {
   };
 
   // post message to #random
-  var anonymousChannel = slack.getChannelByName('random');
+  var anonymousChannel = slack.getChannelByName('general');
   anonymousChannel.postMessage(response);
 }
 
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 3000;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    var quote = "The NSA has built an infrastructure that allows it to intercept almost everything. With this capability, the vast majority of human communications are automatically ingested without targeting. If I wanted to see your emails or your wife's phone, all I have to do is use intercepts. I can get your emails, passwords, phone records, credit cards. I don't want to live in a society that does these sort of things... I do not want to live in a world where everything I do and say is recorded. That is not something I am willing to support or live under.\n\n-Edward Snowden, NSA files source: 'If they want to get you, in time they will', The Guardian, 10 June 2013.";
+    var quote = " BOOTY BOOTY BOOTY The NSA has built an infrastructure that allows it to intercept almost everything. With this capability, the vast majority of human communications are automatically ingested without targeting. If I wanted to see your emails or your wife's phone, all I have to do is use intercepts. I can get your emails, passwords, phone records, credit cards. I don't want to live in a society that does these sort of things... I do not want to live in a world where everything I do and say is recorded. That is not something I am willing to support or live under.\n\n-Edward Snowden, NSA files source: 'If they want to get you, in time they will', The Guardian, 10 June 2013.";
     response.end(quote);
 }
 
